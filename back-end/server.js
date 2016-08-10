fd49ab0eb9b24709bf02d1a6062675bdecdb3dad
Austin Hudson <arhudson7@gmail.com>
2016-08-09 21:42:32 -0400

    added api key hiding

--- back-end/server.js
+++ back-end/server.js
@@ -8,19 +8,20 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-let api_key = "SEM3EEC2DA334F75EF02D2191073E2758F8C";
-let api_secret = 'MDBjYmVkNGE2NGVlMWY4YzczMjdmNGExMGZkYzM5OWU';
+let api_key = process.env.api_key;
+let api_secret = process.env.api_secret; 
 let sem3 = require('semantics3-node')(api_key, api_secret);
 
 app.get('/search', function(request, response){
 
   sem3.products.products_field("search", "necklace");
-
     sem3.products.get_products(
       function(err, products){
         if(err){
+          console.log("Error has occurred", err);
            return;
         }
+        console.log(products);
         response.send(JSON.stringify(products))
     })
   })
